7ca49a8f569195512f3a06f773d2e4edfd7e203d
Valgran23 <aksyra042@example.com>
2026-09-24 16:04:37 +0700

    Fix login route and add vercel config

--- server.js
+++ server.js
@@ -26,6 +26,11 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
+// DITAMBAHKAN: Route /login agar Vercel tidak error saat dipanggil dari Hotspot MikroTik
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
